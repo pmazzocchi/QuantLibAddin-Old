4b686fe585896b42dec9addfc6354d149faeab72
Eric Ehlers <eric.ehlers@reposit.be>
2017-12-30 12:17:11 +0100

    update version numbers

--- QuantLibXL/make_zip.py
+++ QuantLibXL/make_zip.py
@@ -11,7 +11,7 @@ import re
 QLXL = "QuantLibXL"
 VERSION = "1.11.0"
 VERSION_ = "1_11_0"
-VC_VERSION = "vc120"
+VC_VERSION = "vc141"
 QLXL_VERSION = QLXL + "-" + VERSION
 ROOT_DIR = QLXL_VERSION + "\\"
 
